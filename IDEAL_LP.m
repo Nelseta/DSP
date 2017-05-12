e4fea77b7ce8fafb403ab2d5df446d7b35ca14d1
Nelseta <nelson-k2@hotmail.com>
2017-05-12 13:33:51 -0300

    Create IDEAL_LP.m

---
+++ IDEAL_LP.m
@@ -0,0 +1,13 @@
+function [hd]=ideal_lp(wc,M) 
+%Ideal Lowpass  filter  computation 
+%------------------------------------------ 
+%[hd]=ideal_lp(wc,M) 
+%hd=ideal impulse response between 0 and M-1  
+%wc=cutoff frequency in radians 
+%M=length of tne ideal filter 
+% 
+% 
+alpha=(M-1)/2; 
+n=[0:(M-1)]; 
+m=n-alpha+eps; 
+hd=sin(wc*m)./(pi*m);
